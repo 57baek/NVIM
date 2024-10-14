bd1983608ab19c716f81715eea6a6ee9de8f3a0f
HyangMok Baek <57baek@gmail.com>
2024-10-14 00:39:18 -0400

    fugittive keymap setting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -88,3 +88,46 @@ mapKey("vf", "<Plug>(comment_toggle_linewise_visual)", "x")
 
 -- Set up keybinding to toggle Undotree
 mapKey("<leader>u", vim.cmd.UndotreeToggle)
+
+-- -- Open Git status
+mapKey("<leader>gs", ":Git<CR>")
+
+-- Open Git log
+mapKey("<leader>gl", ":Git log<CR>")
+
+-- Git push
+mapKey("<leader>gp", ":Git push origin main<CR>")
+
+-- -- Git pull
+-- mapKey("<leader>gpl", ":Git pull<CR>")
+
+-- -- Git diff
+-- mapKey("<leader>gd", ":Gdiff<CR>")
+
+-- -- Git blame
+-- mapKey("<leader>gb", ":Git blame<CR>")
+
+-- Git add (stage) current file
+mapKey("<leader>ga", ":Git add %<CR>")
+
+-- Git add all (stage all changes)
+mapKey("<leader>gA", ":Git add .<CR>")
+
+-- Git commit (opens commit message buffer)
+mapKey("<leader>gc", function()
+	vim.ui.input({ prompt = "Commit message: " }, function(msg)
+		if msg then
+			vim.cmd(string.format('Git commit -m "%s"', msg))
+		end
+	end)
+end)
+
+-- Git add all and commit (with prompt for commit message)
+mapKey("<leader>gac", function()
+	vim.ui.input({ prompt = "Commit message: " }, function(msg)
+		if msg then
+			vim.cmd("Git add .")
+			vim.cmd(string.format('Git commit -m "%s"', msg))
+		end
+	end)
+end)
